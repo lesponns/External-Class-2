2fa1f4dc02ff52856668b8b54db39cc411edf779
lesponns <adamadakadam1903@gmail.com>
2023-08-15 20:27:49 +0300

    Add project files.

---
+++ SmartPro_33[15-08-23]/Method.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_33_15_08_23_
+{
+    internal class Method
+    {
+        public string YaziGirisi()
+        {
+            string text = string.Empty;
+            bool error = true;
+            do
+            {
+                Console.Write("Değer giriniz: ");
+                text = Console.ReadLine();
+                if (text.Length == 0)
+                {
+                    Console.Write("\nBoş Bırakılamaz.\n");
+                    error = true;
+                }
+                else
+                {
+                    error = false;
+                }
+
+            } 
+            while (error);
+            return text;
+        }
+    }
+}
---
+++ SmartPro_33[15-08-23]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_33_15_08_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Method method = new Method();
+            Console.Write("\nGirdiğiniz değer: {0}",method.YaziGirisi());
+            Console.ReadKey();
+        }
+    }
+}
